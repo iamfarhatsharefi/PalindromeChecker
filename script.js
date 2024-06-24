27234e99b9ce5c4ad5b25c5696478af04e036972
iamfarhatsharefi <sharefifarhat@gmail.com>
2024-06-24 07:40:00 -0700

    trying to solve the remaining linters error

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ document.getElementById('check-btn').addEventListener('click', () => {
   const resultElement = document.getElementById('result');
   const inputValue = inputElement.value.trim();
 
-  if (inputValue === "") {
+  if (inputValue === '') {
     resultElement.textContent = 'Please input a value';
     return;
   }
-    
+  
   const cleanedInput = inputValue.toLowerCase().replace(/[^a-z0-9]/gi, '');
   const reversedInput = cleanedInput.split('').reverse().join('');
-  
+ 
   if (cleanedInput === reversedInput) {
     resultElement.textContent = `${inputValue} is a palindrome`;
   } else {
